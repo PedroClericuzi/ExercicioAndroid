10faa0f92795de2c5837c5f77c077528e4ae8149
PedroClericuzi <pedro.vinicius.batista@gmail.com>
2017-05-26 14:21:59 -0300

    reformulacao de projeto

---
+++ exercicioAndroid/app/src/main/java/com/example/pedroclericuzi/exercicioandroid/activities/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.pedroclericuzi.exercicioandroid.activities;
+import com.example.pedroclericuzi.exercicioandroid.adapter.*;
+import com.example.pedroclericuzi.exercicioandroid.model.*;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ListView;
+
+import com.example.pedroclericuzi.exercicioandroid.R;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    String[] titulo = new String[]{"Titulo 1", "Titulo 2", "Titulo 3"};
+    String[] data = new String[]{"2015", "2016", "2017"};
+    ListView listView;
+    //ArrayAdapter<parserJSON> parseFilms;
+    adapter_parse adapter;
+    ArrayList lista = new ArrayList();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        listView = (ListView) findViewById(R.id.lista_filmes);
+
+        for (int i = 0; i<titulo.length;i++){
+            lista.add(new parserJSON(titulo[i], data[i], "http://google.com"));
+        }
+        listView.setAdapter(new adapter_parse(this, lista));
+    }
+
+    public String downloadJson (){
+        String data = new String();
+
+        return data;
+    }
+}
